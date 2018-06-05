6515c53ee6781a980f2272cba95651342063de99
darongliu <b01901079@ntu.edu.tw>
2018-06-06 01:06:57 +0800

    minored

--- hyperparams.py
+++ hyperparams.py
@@ -34,7 +34,7 @@ class Hyperparams:
     attention_hidden_units = 512
     r = 5 # Reduction factor. Paper => 2, 3, 5
     dropout_rate = 0.1 #rate=0.1 would drop out 10% of input units.
-    attention_mechanism='original' #original #dot
+    attention_mechanism='dot' #original #dot
 
     # training scheme
     num_epochs=300
